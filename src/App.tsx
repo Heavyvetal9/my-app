4c2a562673f71dd7a97148eb100d164b40422971
Heavyvetal9 <buryanov9@yandex.ru>
2025-07-18 15:47:08 +0300

    my-app1

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
-    debugger
+
+    let task1 = [
+        {id: 1, title: 'CSS', isDone: true},
+        {id: 1, title: 'JS', isDone: true},
+        {id: 1, title: 'React', isDone: false}
+    ]
+    let task2 = [
+        {id: 1, title: 'Term', isDone: true},
+        {id: 1, title: 'XXX', isDone: true},
+        {id: 1, title: 'Forum', isDone: false}
+    ]
+
     return (
         <div className="App">
-            <Todolist/>
-            <Todolist/>
-            <Todolist/>
+            <Todolist title={'What to learn'} tasks={task1}/>
+            <Todolist title={'Movie'} tasks={task2}/>
         </div>
     );
 }
